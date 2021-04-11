3201c6d3064710ef4ce6a520c26ad593906a6683
Richa Verma <richa.v1102@gmail.com>
2021-04-11 11:14:33 +0100

    my comiy

---
+++ src/main/java/debug/Main.java
@@ -0,0 +1,14 @@
+package debug;
+
+public class Main<multiply> {
+    public static void main(String[] args){
+        double dollarRate = 1.5913;
+        double rate = 8;
+        double multiply = dollarRate * rate;
+
+        System.out.print("The 8 pound equals to " + multiply + " in dollars");
+
+
+    }
+}
+
